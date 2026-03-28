a978ba86a6e1f8036a4f81ea74b93fcddada4bdc
Amani Boussakour <60305749@udst.edu.qa>
2026-03-29 02:37:37 +0300

    Login page created

---
+++ README.md
@@ -0,0 +1,11 @@
+USER 1:
+username: user
+password: pass
+stored password hash: d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1
+
+
+USER 2:
+username: john
+password: test
+stored password hash: 
+9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08
--- htmlpresentation.js
+++ htmlpresentation.js
@@ -1,11 +1,87 @@
+require('dotenv').config()
+
 const express = require('express')
 const business = require('./business.js')
 const bodyParser= require('body-parser')
 const handlebars = require('express-handlebars')
 const {ObjectId} = require('mongodb');
+const crypto = require('crypto')
+const cookieParser = require('cookie-parser')
 app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+let sessions = {}
+
+function hashPassword(password) {
+    return crypto.createHash('sha256').update(password).digest('hex')
+}
+app.get('/login', (req, res) => {
+    res.render('login', { error: req.query.error })
+})
+
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+
+    const db = await require('./persistence.js').getDb()
+    const user = await db.collection("users").findOne({ username })
+    if (!user || user.password !== hashPassword(password)) {
+        return res.redirect("/login?error=Invalid. Try again: ")
+    }
+    const sessionId = crypto.randomBytes(16).toString("hex")
+
+    sessions[sessionId] = {
+        username,
+        expires: Date.now() + 5 * 60 * 1000
+    }
+    res.cookie("sessionId", sessionId)
+    res.redirect("/")
+})
+
+app.get('/logout', (req, res) => {
+    const sessionId = req.cookies.sessionId
+    delete sessions[sessionId]
+    res.clearCookie('sessionId')
+    res.redirect('/login')
+})
+
+function auth(req, res, next) {
+    const sessionId = req.cookies.sessionId
+    if (!sessionId || !sessions[sessionId]) {
+        //return res.redirect('/login?error=Please login first')
+        return res.redirect('/login')
+    }
+    if (sessions[sessionId].expires < Date.now()) {
+        delete sessions[sessionId]
+        return res.redirect('/login?error=Session expired')
+    }
+    sessions[sessionId].expires = Date.now() + 5 * 60 * 1000
+    req.user = sessions[sessionId].username
+    next()
+}
+
+app.use((req, res, next) => {
+    if (req.path === '/login' || req.path === '/logout') {
+        return next()
+    }
+    auth(req, res, next)
+})
+
+app.use(async (req, res, next) => {
+    const db = await require('./persistence.js').getDb()
+    await db.collection('security_log').insertOne({
+        timestamp: new Date(),
+        username: req.user || 'guest',
+        url: req.url,
+        method: req.method
+    })
+    next()
+})
+
+
 
 app.get('/employee/edit/:id', async (req,res)=>{
     const emp = await business.findEmployee(req.params.id)
