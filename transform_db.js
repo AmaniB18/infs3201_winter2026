ead4c18d3b34ed100a084a3825864526c0f3bece
Amani Boussakour <60305749@udst.edu.qa>
2026-03-28 11:20:29 +0300

    removed unecessary fields + assignment collection

--- transform_db.js
+++ transform_db.js
@@ -10,22 +10,22 @@ async function connectDatabase(){
     return client.db(dbName)
 }
 
-async function addEmployeesArray(shifts) {
+async function addEmployeesArray(shifts){
     const allShifts = await shifts.find({}).toArray();
-    for (let i = 0; i < allShifts.length; i++) {
+    for (let i = 0; i < allShifts.length; i++){
         await shifts.updateOne(
-            { _id: allShifts[i]._id },
-            { $set: { employees: [] } }
+            {_id: allShifts[i]._id},
+            {$set: {employees: []}}
         ); }
 }
 
-async function embedEmployees(shifts, assignments, employees) {
+async function embedEmployees(shifts, assignments, employees){
     const allAssignments = await assignments.find({}).toArray();
-    for (let i = 0; i < allAssignments.length; i++) {
+    for (let i = 0; i < allAssignments.length; i++){
         let assignment = allAssignments[i];
 
-        let emp = await employees.findOne({ employeeId: assignment.employeeId });
-        let shift = await shifts.findOne({ shiftId: assignment.shiftId });
+        let emp = await employees.findOne({employeeId: assignment.employeeId });
+        let shift = await shifts.findOne({shiftId: assignment.shiftId});
         if (emp && shift) {
             await shifts.updateOne(
                 { _id: shift._id },
@@ -33,13 +33,21 @@ async function embedEmployees(shifts, assignments, employees) {
             );
         }}
 }
-async function run() {
+
+async function cleanup(shifts, employees, db){
+    await employees.updateMany({}, {$unset: {employeeId: ""}});
+    await shifts.updateMany({}, {$unset: {shiftId: ""}});
+    await db.collection("assignments").drop();
+}
+
+async function run(){
     const db = await connectDatabase();
     const shifts = db.collection("shifts");
     const assignments = db.collection("assignments");
     const employees = db.collection("employees");
-    await addEmployeesArray(shifts);
-    await embedEmployees(shifts, assignments, employees);
+    //await addEmployeesArray(shifts);
+    //await embedEmployees(shifts, assignments, employees);
+    await cleanup(shifts, employees, db);
 }
 
 run();
