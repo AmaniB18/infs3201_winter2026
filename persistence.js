18732d387a35013b6e6b72f323efd837b7ed6741
Amani Boussakour <60305749@udst.edu.qa>
2026-03-28 21:02:29 +0300

    refactored persistence.js

--- persistence.js
+++ persistence.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { MongoClient } = require('mongodb')
+const {MongoClient, ObjectId} = require('mongodb')
 let client = undefined
 const dbName = 'infs3201_winter2026'
 async function connectDatabase(){
@@ -31,7 +31,9 @@ async function getAllEmployees() {
  */
 async function findEmployee(empId) {
     const db = await getDb()
-    let result= await db.collection('employees').findOne({ employeeId: empId })
+    if (!ObjectId.isValid(empId)){
+        return undefined;}
+    let result= await db.collection('employees').findOne({_id: new ObjectId(empId)})
     return result
     
 }
@@ -43,7 +45,9 @@ async function findEmployee(empId) {
  */
 async function findShift(shiftId) {
     const db = await getDb()
-    return await db.collection('shifts').findOne({ shiftId: shiftId })
+    if (!ObjectId.isValid(shiftId)){
+        return undefined;}
+    return await db.collection('shifts').findOne({_id: new ObjectId(shiftId)})
 }
 
 /**
@@ -53,23 +57,11 @@ async function findShift(shiftId) {
  */
 async function getEmployeeShifts(empId) {
     const db = await getDb()
-    let assignments = await db.collection('assignments').find({ employeeId: empId }).toArray()
-
-    let shiftDetails = []
-    for (let a of assignments) {
-        let shift = await db.collection('shifts').findOne({shiftId: a.shiftId})
-        if (shift){
-            shiftDetails.push(shift)
-        }
-    }
-
-    return shiftDetails
+    if (!ObjectId.isValid(empId)){
+        return [];}
+    let shifts = await db.collection('shifts').find({employees: new ObjectId(empId)}).toArray()
+    return shifts
 }
-
-
-
-
-
 
 /**
  * Add a new employee record to the system. The empId is automatically generated based
@@ -78,18 +70,9 @@ async function getEmployeeShifts(empId) {
  */
 async function addEmployeeRecord(emp) {
     const db = await getDb()
-    let employees = await db.collection('employees').find({}).toArray()
-    let maxId = 0
-    for (let e of employees) {
-        let num = Number(e.employeeId.slice(1))
-        if (num > maxId) maxId = num
-    }
-    emp.employeeId = `E${String(maxId+1).padStart(3,'0')}`
     await db.collection('employees').insertOne(emp)
 
 }
-
-
 
 module.exports = {
     getAllEmployees, findEmployee,
